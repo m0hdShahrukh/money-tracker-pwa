c9f9c2e330e89447cea4c20bd3495d89e80a5177
m0hdShahrukh <mohdsharukh23@gmail.com>
2025-06-18 10:49:51 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      manifest: {
+        name: 'Money Tracker PWA',
+        short_name: 'MoneyTracker',
+        description: 'Your personal money tracker',
+        theme_color: '#0F172A',
+        background_color: '#0F172A',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+})
